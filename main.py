732ebd069e7a0471a9a3187a8d4b89f38a96a214
Diane Lee <dplee86@gmail.com>
2017-09-25 13:05:29 -0500

    web ceasar program through processing form

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form ="""
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="POST">
+            <label>Rotate by:
+                <input name="rot" type="text" value="0"/>
+            </label>
+            <br>
+            <label>
+                <textarea name="text"></textarea>
+            </label>
+            <br>
+            <input type="submit"/>
+        </form>
+    </body>
+
+    </html>
+    """
+
+    
+@app.route("/")
+def index():
+    return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rotation=int(request.form['rot'])
+    text_to_encrypt=request.form['text']
+    
+    encrypted_message = rotate_string(text_to_encrypt,rotation)
+
+    return encrypted_message
+
+
+app.run()
